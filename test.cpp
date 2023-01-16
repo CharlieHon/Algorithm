2896079fb4b55bdf7c5b241190a234c325bcdc11
Charlie <2295509496@qq.com>
2023-01-16 19:21:24 +0800

    基础排序算法

--- test.cpp
+++ test.cpp
@@ -16,26 +16,25 @@ void bubbleSort(int *a, const int n){
 
 void selectSort(int *a, const int n){
     for(int i=0; i<n-1; ++i){
-        int k = i;
+        int min = i;
         for(int j=i+1; j<n; ++j){
-            if(a[j] < a[k])
-                k = j;
+            if(a[j] < a[min])
+                min = j;
         }
-        int temp = a[i];
-        a[i] = a[k];
-        a[k] = temp;
+        int tmp = a[i];
+        a[i] = a[min];
+        a[min] = tmp;
     }
 }
 
 void insertSort(int *a, const int n){
-    for(int i=1; i<=n; ++i){
-        int temp = a[i];
-        int j = i;
-        while(j > 0 && temp < a[j-1]){
+    for(int i=1; i<n; ++i){
+        int j = i, tmp = a[i];
+        while(j > 0 && tmp < a[j-1]){
             a[j] = a[j-1];
             --j;
         }
-        a[j] = temp;
+        a[j] = tmp;
     }
 }
 
@@ -48,6 +47,53 @@ void print_a(int  *a, const int n){
     printf("\n");
 }
 
+const int maxn = 101;
+//将数组 A 在区间 [L1, R1] 和 [L2, R2] 的两个有序子区间合并为连续的有序序列
+void merge(int *A, int L1, int R1, int L2, int R2){
+    int i = L1, j = L2;
+    int tmp[maxn], index = 0;
+    while(i <= R1 && j <= R2){
+        if(A[i] <= A[j])
+            tmp[index++] = A[i++];
+        else
+            tmp[index++] = A[j++];
+    }
+    while(i <= R1)  tmp[index++] = A[i++];
+    while(j <= R2)  tmp[index++] = A[j++];
+    for(int i=0; i<index; ++i)
+        A[L1+i] = tmp[i];
+}
+//归并排序，[left, right] 为下标从0开始的区间
+void mergeSort(int *A, int left, int right){
+    if(left < right){
+        int mid = (left + right) / 2;
+        mergeSort(A, left, mid);
+        mergeSort(A, mid+1, right);
+        merge(A, left, mid, mid+1, right);
+    }
+}
+
+int Partition(int *A, int left, int right){
+    int tmp = A[left];
+    while(left < right){
+        while(left < right && A[right] > tmp)  --right;
+        A[left] = A[right];
+        while(left < right && A[left] <= tmp)   ++left;
+        A[right] = A[left];
+    }
+    A[left] = tmp;  //把 tmp 放到 left 与 right 相遇的地方
+    return left;    //返回相遇的下标
+}
+
+//快速排序，递归实现
+void quickSort(int *A, int left, int right){
+    if(left < right){
+        int pos = Partition(A, left, right);
+        quickSort(A, left, pos-1);
+        quickSort(A, pos+1, right);
+    }
+}
+
 int main()
 {
     int a[] = {1, 5, 6, 32, 4, 8, 9, 2, 0};
@@ -56,7 +102,9 @@ int main()
     // bubbleSort(a, n);
     // selectSort(a, n);
     // insertSort(a, n);
-    sort(a, a+n);   //sort(数组首元素地址，尾元素后一位地址，排序规则)
+    // mergeSort(a, 0, n-1);
+    quickSort(a, 0, n-1);
+    // sort(a, a+n);   //sort(数组首元素地址，尾元素后一位地址，排序规则)
     print_a(a, n);
     return 0;
 }
